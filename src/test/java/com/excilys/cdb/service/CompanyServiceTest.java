f73072cb614928db24964fd36bc229dcf1dc91dd
jfaverie <jean.faverie@gmail.com>
2016-04-26 18:13:47 +0200

    TestUnitaire

---
+++ src/test/java/com/excilys/cdb/service/CompanyServiceTest.java
@@ -0,0 +1,27 @@
+package com.excilys.cdb.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CompanyServiceTest {
+
+    private CompanyService service;
+    
+    @Before
+    public void setUp() throws Exception {
+        service = CompanyService.getInstance();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getById() {
+        Company company = 
+    }
+
+}
---
+++ src/test/java/com/excilys/cdb/service/ComputerServiceTest.java
@@ -0,0 +1,24 @@
+package test.java.com.excilys.cdb.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ComputerServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
